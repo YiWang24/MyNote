d11a9fa8a83a8419773a49342b8f6e6470ee35c0
YiWang24 <yiwang2457@gmail.com>
2025-01-31 15:59:36 -0500

    change note category page

--- frontend/src/components/note/NewNoteCategory.jsx
+++ frontend/src/components/note/NewNoteCategory.jsx
@@ -1,6 +1,19 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNoteContext } from "@/lib/noteContext";
+import { Particles } from "@/components/ui/particles";
+import { LineShadowText } from "@/components/ui/line-shadow-text";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 const NewNoteCategory = () => {
   const {
     categories,
@@ -20,7 +33,6 @@ const NewNoteCategory = () => {
         : [...prev, categoryId]
     );
   };
-
   const handleDelete = async () => {
     await deleteCategories(selectedCategories);
     setSelectedCategories([]);
@@ -33,73 +45,83 @@ const NewNoteCategory = () => {
     createCategory(newCategory);
     setNewCategory("");
   };
+  useEffect(() => {
+    if (isCategoryModalOpen) {
+      setSelectedCategories([]);
+    }
+  }, [isCategoryModalOpen]);
   return (
     <>
       {isCategoryModalOpen && (
         <div className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50">
-          <div className="bg-white rounded-lg p-8 w-full max-w-md z-50 m-4">
-            <h2 className="text-2xl font-bold mb-4">Categories Management</h2>
-
-            {/* Current Categories */}
-            <div className="mb-6">
-              <h3 className="text-lg font-semibold mb-2">Current Categories</h3>
-              <div className="flex flex-wrap gap-2">
-                {categories?.map((category) => (
-                  <span
-                    key={category._id}
-                    onClick={() => handleCategoryClick(category._id)}
-                    className={`px-3 py-1 rounded-full text-sm cursor-pointer
+          <Particles
+            className="absolute inset-0 z-0"
+            quantity={100}
+            ease={80}
+            color="#ffffff"
+            refresh
+          />
+          <Card className="w-[350px] sm:w-[450px] md:w-[550px] lg:w-[650px]">
+            <CardHeader>
+              <CardTitle>
+                Categories{" "}
+                <LineShadowText className="italic" shadowColor="black">
+                  Management
+                </LineShadowText>
+              </CardTitle>
+              <CardDescription>Add or Delete your categories</CardDescription>
+            </CardHeader>
+            <CardContent>
+              {/* current categories */}
+              <div className="mb-6">
+                <h3 className="text-lg font-semibold mb-2">
+                  Current Categories
+                </h3>
+                <div className="flex flex-wrap gap-2">
+                  {categories?.map((category) => (
+                    <span
+                      key={category._id}
+                      onClick={() => handleCategoryClick(category._id)}
+                      className={`px-3 py-1 rounded-full  cursor-pointer
                       ${
                         selectedCategories.includes(category._id)
-                          ? "bg-red-100 text-red-700"
-                          : "bg-gray-100 text-gray-700"
+                          ? "whitespace-nowrap rounded-full bg-pink-100  text-sm text-pink-500"
+                          : "whitespace-nowrap rounded-full border border-purple-500  text-sm text-purple-700"
                       }`}
-                  >
-                    {category.type}
-                  </span>
-                ))}
+                    >
+                      {category.type}
+                    </span>
+                  ))}
+                </div>
               </div>
-            </div>
-
-            {/* New Category Input */}
-            <form
-              className="flex items-center gap-2 mb-4"
-              onSubmit={handleSubmit}
-            >
-              <input
-                type="text"
-                value={newCategory}
-                onChange={(e) => setNewCategory(e.target.value)}
-                className="flex-1 rounded-md border-gray-300 shadow-sm focus:border-red-500 focus:ring-red-500"
-                placeholder="New category"
-              />
-              <button
-                type="submit"
-                className="px-3 py-2 bg-gray-100 rounded-md hover:bg-gray-200"
-              >
-                +
-              </button>
-            </form>
-
-            <div className="flex justify-end space-x-4 pt-4">
-              {selectedCategories.length > 0 && (
-                <button
-                  type="button"
-                  onClick={handleDelete}
-                  className="px-4 py-2 text-sm font-medium text-red-600 hover:text-red-500"
-                >
-                  Delete Selected
-                </button>
-              )}
-              <button
-                type="button"
-                onClick={controlCategoryModal}
-                className="px-4 py-2 text-sm font-medium text-gray-700 hover:text-gray-500"
-              >
+              {/* add new category */}
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-col  space-y-1.5">
+                  <Label htmlFor="name">Category</Label>
+                  <div className="flex w-full items-center justify-around  gap-2">
+                    <Input
+                      id="name"
+                      value={newCategory}
+                      onChange={(e) => setNewCategory(e.target.value)}
+                      placeholder="New category"
+                      className="flex-1"
+                    />
+                    <Button>+</Button>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline" onClick={controlCategoryModal}>
                 Cancel
-              </button>
-            </div>
-          </div>
+              </Button>
+              {selectedCategories.length > 0 && (
+                <Button variant="destructive" onClick={handleDelete}>
+                  Delete Selected
+                </Button>
+              )}
+            </CardFooter>
+          </Card>
         </div>
       )}
     </>
